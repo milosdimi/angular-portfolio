bb256d09814a4f0c01fb15911d98dae293f4a18a
milosdimi <132084913+milosdimi@users.noreply.github.com>
2026-01-16 19:37:15 +0100

    component added & routing header, footer, home, imprint, privacy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { ImprintComponent } from './pages/imprint/imprint.component';
+import { PrivacyComponent } from './pages/privacy/privacy.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'impressum', component: ImprintComponent, title: 'Impressum' },
+  { path: 'datenschutz', component: PrivacyComponent, title: 'Datenschutz' },
+
+  // redirect unknown routes to home
+  { path: '**', redirectTo: '' },
+];
