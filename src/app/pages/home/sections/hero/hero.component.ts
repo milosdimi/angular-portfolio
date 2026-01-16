da73a4530ec9ad721bc79d08c1c60761b7f0e57a
milosdimi <132084913+milosdimi@users.noreply.github.com>
2026-01-16 21:06:05 +0100

    font, header, logo, hero update

--- src/app/pages/home/sections/hero/hero.component.ts
+++ src/app/pages/home/sections/hero/hero.component.ts
@@ -5,8 +5,15 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.scss'
+  styleUrl: './hero.component.scss',
 })
 export class HeroComponent {
-
+  onMove(e: MouseEvent) {
+    const el = e.currentTarget as HTMLElement;
+    const rect = el.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    el.style.setProperty('--mx', `${x}px`);
+    el.style.setProperty('--my', `${y}px`);
+  }
 }
