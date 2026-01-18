81644038546560e947e8483dfd5fdf0949d6c3a3
milosdimi <132084913+milosdimi@users.noreply.github.com>
2026-01-18 19:49:19 +0100

    about added

--- src/app/pages/home/sections/about/about.component.ts
+++ src/app/pages/home/sections/about/about.component.ts
@@ -3,10 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrl: './about.component.scss',
 })
 export class AboutComponent {
-
+  about = {
+    title: 'Über mich',
+    location: 'Kufstein, Tirol – offen für Remote-Arbeit',
+    mindset: 'Neugierig, offen für neue Technologien und kontinuierliche Weiterentwicklung',
+    approach:
+      'Analytisch, lösungsorientiert und detailverliebt – mit Fokus auf Qualität, Performance und UX',
+  };
 }
